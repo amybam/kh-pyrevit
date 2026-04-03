32de32281896a23e9de04b35d8526e553926fe05
Kevin Huberty <kevin.huberty@arup.com>
2026-04-03 10:53:48 -0700

    Update script description to reflect functionality for drawing Revision Cloud

--- KHTolls.tab/Revisions.panel/Cloud Selected.pushbutton/script.py
+++ KHTolls.tab/Revisions.panel/Cloud Selected.pushbutton/script.py
@@ -1,18 +1,1 @@
-"""Calculates total volume of all walls in the model."""
-
-from Autodesk.Revit import DB
-
-doc = __revit__.ActiveUIDocument.Document
-
-wall_collector = DB.FilteredElementCollector(doc)\
-    .OfCategory(DB.BuiltInCategory.OST_Walls)\
-        .WhereElementIsNotElementType()
-        
-total_volume = 0.0
-
-for wall in wall_collector:
-    vol_param = wall.get_Parameter(DB.BuiltInParameter.HOST_VOLUME_COMPUTED)
-    if vol_param:
-        total_volume += vol_param.AsDouble()
-        
-print("Total Volume of Walls: {}".format(total_volume))
+"""Draws Revision Cloud around selected elements"""
